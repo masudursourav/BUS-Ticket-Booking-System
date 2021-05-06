a10d18dc58a14e5afb2e39d84be6219fe070d2a5
Masudur Rahman Sourav <66933835+masudursourav@users.noreply.github.com>
2021-05-06 23:25:50 +0600

    Add files via upload

---
+++ decleration.c
@@ -0,0 +1,10 @@
+int changeprize(int);
+void reservation(int *,int,int );
+int choice1(void);
+void cancel(int *);
+void ticket1(int choice,char name[10],int id2,int price);
+void ticket2(int choice,char name[10],int id2,int price);
+void ticket3(int choice,char name[10],int id2,int price);
+int cticket(void);
+int ticket(void);
+void details(void);
